fbd9454db6bea03151b2d43e59910d0da821374f
karan <karansofat89@gmail.com>
2015-08-01 16:12:36 +0530

    Update gmail.php

--- gmail.php
+++ gmail.php
@@ -94,7 +94,31 @@ Dont forget to enable api.
 
 For enabling api go to the google+console and enable it
 
+======================OR=========================================
+<html lang="en">
+  <head>
+    <meta name="google-signin-scope" content="profile email">
+    <meta name="google-signin-client_id" content="176786682817-2h97q96los8ekcotoofbos5n1pcqd78u.apps.googleusercontent.com">
+    <script src="https://apis.google.com/js/platform.js" async defer></script>
+  </head>
+  <body>
+    <div class="g-signin2" data-onsuccess="onSignIn" data-theme="dark"></div>
+    <script>
+      function onSignIn(googleUser) {
+        // Useful data for your client-side scripts:
+        var profile = googleUser.getBasicProfile();
+        console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+        console.log("Name: " + profile.getName());
+        console.log("Image URL: " + profile.getImageUrl());
+        console.log("Email: " + profile.getEmail());
 
+        // The ID token you need to pass to your backend:
+        var id_token = googleUser.getAuthResponse().id_token;
+        console.log("ID Token: " + id_token);
+      };
+    </script>
+  </body>
+</html>
 
 
 
